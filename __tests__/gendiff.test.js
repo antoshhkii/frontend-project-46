f333b24fa66c30ab43b85d3cc4d8fc8b62a88877
antoshhkii <velikii.nord@gmail.com>
2023-02-22 14:04:09 +0300

    minor fixes

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,14 +1,56 @@
 import { test, expect } from '@jest/globals';
-import genDiff from '../src/index.js';
+import path from 'path';
 import { getFile } from '../src/parse.js';
+import genDiff from '../src/index.js';
 
-const firstExpectedFile = getFile('./__fixtures__/expected_files/gendiff1-2.txt');
-const secondExpectedFile = getFile('./__fixtures__/expected_files/gendiff3-4.txt');
+const makePath = (file) => {
+  const extension = path.extname(file);
+  let dirname;
 
-const firstStrPath = ['./__fixtures__/json_test_files/file1.json', './__fixtures__/json_test_files/file2.json'];
-const secondStrPath = ['./__fixtures__/json_test_files/file3.json', './__fixtures__/json_test_files/file4.json'];
+  switch (extension) {
+    case '.txt':
+      dirname = `./__fixtures__/expected_files/${file}`;
+      break;
 
-test('genDiff.json', () => {
-  expect(genDiff(firstStrPath[0], firstStrPath[1])).toEqual(firstExpectedFile);
-  expect(genDiff(secondStrPath[0], secondStrPath[1])).toEqual(secondExpectedFile);
+    case '.json':
+      dirname = `./__fixtures__/json_test_files/${file}`;
+      break;
+
+    case '.yaml':
+    case '.yml':
+      dirname = `./__fixtures__/yaml_test_files/${file}`;
+      break;
+
+    default:
+      console.log(`File [${file}] does not have a valid parser!`);
+      break;
+  }
+  if (extension === '.txt') return getFile(dirname);
+  return dirname;
+};
+
+test('JSON files are parsing', () => {
+  expect(genDiff(makePath('file1.json'), makePath('file2.json')))
+    .toEqual(makePath('gendiffJSON1-2.txt'));
+
+  expect(genDiff(makePath('file3.json'), makePath('file4.json')))
+    .toEqual(makePath('gendiffJSON3-4.txt'));
 });
+
+// test('YAML files are parsing', () => {
+//   expect(genDiff(makePath('file1.yaml'), makePath('file2.yaml')))
+//     .toEqual(makePath('gendiffYAML1-2.txt'));
+//   expect(genDiff(makePath('file1.yml'), makePath('file2.yml')))
+//     .toEqual(makePath('gendiffYAML3-4.txt'));
+// });
+
+//  test('Throw Errors for parsers', () => {
+// expect(genDiff(makePath('file1.yaml'), makePath('file2.yaml')))
+// .toEqual(makePath('gendiffYAML1-2.txt'));
+//  expect(genDiff(makePath('file1.yaml'), makePath('file2.yaml')))
+// .toEqual(makePath('gendiffYAML1-2.txt'));
+//  expect(genDiff(makePath('file1.yaml'), makePath('file2.yaml')))
+// .toEqual(makePath('gendiffYAML1-2.txt'));
+//  expect(genDiff(makePath('file1.yaml'), makePath('file2.yaml')))
+// .toEqual(makePath('gendiffYAML1-2.txt'));
+//  });
