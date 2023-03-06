7163ba84f6edf27a62821039cacba81ca9fca714
antoshhkii <velikii.nord@gmail.com>
2023-03-06 14:23:28 +0300

    minor fixes

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,11 +9,13 @@ test('JSON files are parsing', () => {
     .toEqual(makePath('gendiffJSON3-4.txt'));
 });
 
-test('YAML files are parsing', () => {
+test('YAML & YML files are parsing', () => {
   expect(genDiff(makePath('file1.yaml'), makePath('file2.yaml')))
     .toEqual(makePath('gendiffYAML1-2.txt'));
   expect(genDiff(makePath('file1.yml'), makePath('file2.yml')))
     .toEqual(makePath('gendiffYAML3-4.txt'));
+  expect(genDiff(makePath('file3.yaml'), makePath('file3.yml')))
+    .toEqual(makePath('gendiffYAML-YML.txt'));
 });
 
 //  test('Throw Errors for parsers', () => {
