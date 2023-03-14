c97a09c0b9da8aff2c72109479240723a2a8309d
antoshhkii <velikii.nord@gmail.com>
2023-03-14 14:39:09 +0300

    minor fixes

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,4 +1,4 @@
-import { stringify } from '../tree.js';
+import { toString } from '../tree.js';
 
 const labels = {
   deleted: '- ',
@@ -7,4 +7,36 @@ const labels = {
   unchanged: '  ',
 };
 
-const makeDiffString = (indent, type, key, value, depth) => `${indent}${labels[type]}${key}: ${stringify(value, depth + 1)}`;
+const makeDiffString = (indent, type, key, value, depth) => `${indent}${labels[type]}${key}: ${toString(value, depth + 1)}`;
+
+const getStylishLine = (status, indent, key, value, depth) => {
+  switch (status) {
+    case 'deleted':
+      return makeDiffString(indent, 'deleted', key, value, depth);
+    case 'added':
+      return makeDiffString(indent, 'added', key, value, depth);
+    case 'changed':
+      return `${makeDiffString(indent, 'deleted', key, value.oldValue, depth)}\n${makeDiffString(indent, 'added', key, value.newValue, depth)}`;
+    default:
+      return makeDiffString(indent, 'unchanged', key, value, depth);
+  }
+};
+
+const stylish = (tree, spacesCount = 4, replacer = ' ') => {
+  const iter = (el, depth) => {
+    const indentSize = depth * spacesCount;
+    const indent = replacer.repeat(indentSize - 2);
+    const bracketIndent = replacer.repeat(indentSize - spacesCount);
+
+    const lines = el.map(({ key, value, status }) => {
+      if (status === 'nested') {
+        return `${indent}${labels.nested}${key}: ${iter(value, depth + 1)}`;
+      }
+      return getStylishLine(status, indent, key, value, depth);
+    });
+    return ['{', ...lines, `${bracketIndent}}`].join('\n');
+  };
+  return iter(tree, 1);
+};
+
+export default stylish;
